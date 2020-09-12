72115000e2d91e702e06d136c4a9e06f49ddb569
SergKV <sergkushnirenko@gmail.com>
2020-09-13 00:34:40 +0200

    added restaurant table

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,52 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant
+                {
+                    Id = 1,
+                    Name = "Scotts's Pizza",
+                    Location = "Maryland",
+                    Cuisine = CuisineType.Italian
+                },
+                new Restaurant
+                {
+                    Id = 2,
+                    Name = "Cinnamon Club",
+                    Location = "London",
+                    Cuisine = CuisineType.Indian
+                },
+                new Restaurant
+                {
+                    Id = 3,
+                    Name = "La Costa",
+                    Location = "California",
+                    Cuisine = CuisineType.Mexican
+                }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from x in restaurants
+                   orderby x.Name
+                   select x; ;
+        }
+    }
+}
